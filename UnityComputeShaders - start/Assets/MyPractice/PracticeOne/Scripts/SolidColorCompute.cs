96bb4ab63cf1e63529cfb7722dc41786780fdb0c
Sami <caspertsky@gmail.com>
2026-02-23 22:12:09 +0500

    Day-One-Complete

---
+++ UnityComputeShaders - start/Assets/MyPractice/PracticeOne/Scripts/SolidColorCompute.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+
+public class SolidColorCompute : MonoBehaviour
+{
+    [SerializeField] ComputeShader computeShader;
+    [SerializeField] string kernelName = "SolidRed";
+    [SerializeField] int texResolution = 256;
+    
+    Renderer _rend;
+    RenderTexture _outputRenderTexture;
+    int _kernelHandle;
+    
+    // Start is calleds once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _outputRenderTexture = new RenderTexture(texResolution, texResolution, 0);
+        _outputRenderTexture.enableRandomWrite = true;
+        _outputRenderTexture.Create();
+        
+        _rend = GetComponent<Renderer>();
+        _rend.enabled = true;
+        
+        InitShader();
+    }
+
+    private void InitShader()
+    {
+        _kernelHandle = computeShader.FindKernel(kernelName);
+        computeShader.SetInt("_texResolution", texResolution);
+        computeShader.SetTexture(_kernelHandle, "Result", _outputRenderTexture);
+        _rend.material.SetTexture("_MainTex", _outputRenderTexture);
+        
+        DispatchShader(texResolution, texResolution);
+    }
+
+    private void DispatchShader(int x, int y)
+    {
+        computeShader.Dispatch(_kernelHandle, x, y, 1);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyUp(KeyCode.U))
+        {
+            DispatchShader(texResolution/8, texResolution/8);
+        }*/
+    }
+}
